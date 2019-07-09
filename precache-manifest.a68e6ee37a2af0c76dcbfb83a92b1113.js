b6e6a8d88057d6731e9e0262293918ad13b4dd5b
Jose Padron F <josepadron.go@gmail.com>
2019-07-09 13:30:08 -0500

    Updates

---
+++ precache-manifest.a68e6ee37a2af0c76dcbfb83a92b1113.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd3666204f7b31af7611d10e2f3af93",
+    "url": "/React-redux/index.html"
+  },
+  {
+    "revision": "03458b6a7bb353482c18",
+    "url": "/React-redux/static/css/main.e702253b.chunk.css"
+  },
+  {
+    "revision": "0ae67aca9f7cbe4b9cda",
+    "url": "/React-redux/static/js/2.229aa132.chunk.js"
+  },
+  {
+    "revision": "03458b6a7bb353482c18",
+    "url": "/React-redux/static/js/main.ca0b41a8.chunk.js"
+  },
+  {
+    "revision": "b31350020e270378225b",
+    "url": "/React-redux/static/js/runtime~main.bfbd25a1.js"
+  },
+  {
+    "revision": "31af7c324d4c060665c29e4acaa78634",
+    "url": "/React-redux/static/media/add.31af7c32.svg"
+  },
+  {
+    "revision": "77d53e314da73a9494e96d75ad3cfe0d",
+    "url": "/React-redux/static/media/delete.77d53e31.svg"
+  },
+  {
+    "revision": "933499a058fd9d3cb6b35da408d3c6df",
+    "url": "/React-redux/static/media/edit.933499a0.svg"
+  },
+  {
+    "revision": "2774f65201814c9c470c02c6e0612e1d",
+    "url": "/React-redux/static/media/error.2774f652.png"
+  }
+]);
